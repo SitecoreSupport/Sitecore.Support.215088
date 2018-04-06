3374c3c1514e1dc043f4578d6c9db8bb2605d0bb
DK\alu <alu@sitecore.net>
2018-04-06 13:48:14 +0300

    Initial commit

---
+++ src/Sitecore.Support.215088/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.215088")]
+[assembly: AssemblyProduct("Sitecore.Support.215088")]
+[assembly: ComVisible(false)]
